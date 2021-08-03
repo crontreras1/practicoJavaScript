be03a3b8ccb1ed6940f8177f2f8bf37c84e781b1
crontreras1 <cr.ontreras@gmail.com>
2021-08-03 11:41:27 -0500

    realicé cupones de descuento en el Js y html

--- descuentos.js
+++ descuentos.js
@@ -30,3 +30,39 @@ function onClickButtonPriceDiscount() {
      porcentajePrecioConDescuento,
      precioConDescuento,
  })*/
+
+ const coupons = [
+     "SoyBatman",
+     "TengoHambre",
+     "EsUnSecreto",
+ ];
+
+ const inputCoupon = document.getElementById("inputCoupon");
+ const couponValue = inputCoupon.value;
+
+ function onClickButtonPriceDiscount() {
+     const inputPrecio = document.getElementById("inputPrecio");
+     const priceValue = inputPrecio.value;
+
+     const inputCoupon = document.getElementById("inputCoupon");
+     const couponValue = inputCoupon.value;
+
+     let descuento;
+
+     switch(couponValue) {
+         case "SoyBatman":
+             descuento =15;
+         break;
+         case "TengoHambre":
+             descuento = 30;
+         break;
+         case "EsUnSecreto":
+             descuento = 25;
+         break;
+     }
+
+     const precioConDescuento = calcularPrecioConDescuento(priceValue, descuento);
+
+     const resultP = document.getElementById("resultP");
+     resultP.innerText = "El precio de tus chanclas con descuento es de " + precioConDescuento;
+ }
